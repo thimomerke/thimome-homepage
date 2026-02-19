b99ea079715a1ac5928afc032c35464ef6caadc9
Thimo Merke <thimomerke@gmail.com>
2026-02-19 12:03:55 +0100

    Resumee updates

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ class AboutMe extends Component {
     const unima = "<a href='https://www.uni-mannheim.de' target='_blank'>University of Mannheim</a>"
     const cbs = "<a href='https://www.cbs.dk/en' target='_blank'>Copenhagen Business School</a>"
     const linkedin = "<a href='https://www.linkedin.com/in/thimo-merke/' target='_blank'>LinkedIn</a>"
-    const email = "<a href='mailto:thimo.merke@students.uni-mannheim.de' target='_blank'>thimo.merke@students.uni-mannheim.de</a>"
+    const email = "<a href='mailto:mail@thimo.me' target='_blank'>mail@thimo.me</a>"
     return (
       <div className="section" id="about">
         <Container>
@@ -22,8 +22,8 @@ class AboutMe extends Component {
           <Row className="section-content">
             <Col className="about-text">
                 <p>I'm a graduate student of business at the {Parser(unima)} and {Parser(cbs)}.</p>
-                <p>My main reasearch interests are the techno-economic aspects of renewable energy, carbon removal and other sustainability-related topics. I also do some other, personal coding projects on the side.</p>
-                <p>In my free time, if I'm not at my PC, I like reading, learning languages, and doing sports such as hiking, biking, running and pretty much everything outdoors. <br />I'm also a huge space nerd #OccupyMars</p>
+                <p>My main reasearch interests are the techno-economic aspects of renewable energy, carbon removal and other energy-related topics. I also do some other, personal coding projects on the side.</p>
+                <p>I'm an avid reader, love learning languages, and doing sports such as hiking, biking, running and pretty much everything outdoors. <br />I'm also a huge space nerd #OccupyMars</p>
                 <p>You want to talk? Let's connect on {Parser(linkedin)}, or just shoot me an e-mail at {Parser(email)}</p>
             </Col>
             <Col className="about-photo">
--- src/components/ExperiencePanel.js
+++ src/components/ExperiencePanel.js
@@ -62,10 +62,17 @@ function TabPanel(props) {
                     </ul>
                 </div>
                 <div className="item">
-                    <span className="job-title">Intern ESG Expert Group</span><span className="job-company"> &#47;&#47; KPMG</span>
-                    <div className="job-time">January 2024 - March 2024</div>
+                    <span className="job-title">Intern Private Equity / Inhouse Consulting</span><span className="job-company"> &#47;&#47; Aurelius</span>
+                    <div className="job-time">July 2025 - October 2025</div>
                     <ul className="job-description">
-                        As an intern with the ESG Expert Group, I worked on projects regarding the implementation of CSRD and other EU sustainability regulations at various large, global companies.
+                        At Aurelius, I worked with various portfolio companies (aerospace, FMCG) on projects regarding regulatory compliance (e.g. CSRD) and procurement.
+                    </ul>
+                </div>
+                <div className="item">
+                    <span className="job-title">Intern ESG Expert Group</span><span className="job-company"> &#47;&#47; KPMG</span>
+                    <div className="job-time">January 2025 - March 2025</div>
+                    <ul className="job-description">
+                        As an intern with the ESG Expert Group, I worked on projects regarding the implementation of CSRD and other EU regulations at various large, global companies.
                     </ul>
                 </div>
                 <div className="item">
@@ -120,14 +127,14 @@ function TabPanel(props) {
                     <span className="job-title">M.Sc. Management</span><span className="job-company"> &#47;&#47; University of Mannheim, Germany</span>
                     <div className="job-time">Since September 2023</div>
                     <ul className="job-description">
-                        I continue my studies as a graduate student at the University of Mannheim. My interests are in sustainable management, finance and accounting
+                        I continue my studies as a graduate student at the University of Mannheim. My interests are in sustainable management, finance and accounting. I was honored to be member of the Dean's List <a href="https://www.mannheim-business-school.com/en/validation/?credential=101d2803-097c-4df3-b402-db798665b1b2" target="_blank" rel="noreferrer">2024</a> and <a href="https://www.mannheim-business-school.com/en/validation/?credential=3945c0bb-584b-402f-8546-f5a70798e87a" target="_blank" rel="noreferrer">2025</a>.
                     </ul>
               </div>
               <div className="item">
                     <span className="job-title">Management</span><span className="job-company"> &#47;&#47; Copenhagen Business School, Denmark</span>
                     <div className="job-time">September 2024 - December 2024</div>
                     <ul className="job-description">
-                        In addition to my studies at the University of Mannheim, I'm spending a semester at CBS with a minor in ESG accounting and sustainable finance
+                        In addition to my studies at the University of Mannheim, I'm spending a semester at CBS with a minor in accounting, regulation and sustainable finance.
                     </ul>
               </div>
               <div className="item">
@@ -137,14 +144,14 @@ function TabPanel(props) {
                         I graduated from the University of Mannheim with a Degree in Business Administration, with a focus on in management, finance and sustainability. My bachelor's thesis, 
                         <span style={{fontStyle: "italic"}}>
                           "Examining the Viability of Net-Negative Carbon Removal Techniques: An Economic and Environmental Evaluation"
-                        </span>, can be found <a href="https://github.com/thimomerke/bachelor-thesis/blob/main/BachelorThesis061023.pdf" target="_blank" rel="noreferrer">here</a>
+                        </span>, can be found <a href="https://github.com/thimomerke/bachelor-thesis/blob/main/BachelorThesis061023.pdf" target="_blank" rel="noreferrer">here.</a>
                     </ul>
                 </div>
                 <div className="item">
                     <span className="job-title">Commerce</span><span className="job-company"> &#47;&#47; Queen's University, Kingston, Canada</span>
                     <div className="job-time">September 2021 - December 2021</div>
                     <ul className="job-description">
-                        I studied at Smith School of Business for one semester, where I focused on Management and Sustainability topics
+                        I studied at Smith School of Business for one semester, where I focused on Management and Entrepreneurship topics
                     </ul>
                 </div>
             </div>
